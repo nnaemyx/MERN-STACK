c26e9b0c3ef84ea4cc4a2d67694fa9ad89daa2d0
tezon <tezonteam@gmail.com>
2022-07-30 23:38:32 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -30,3 +30,16 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => {
     console.log(err)
   }) 
+
+  // Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
