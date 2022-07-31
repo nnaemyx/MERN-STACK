a4c7a39eefd1af884054a40bccd4eac09c087d9c
tezon <tezonteam@gmail.com>
2022-07-31 00:46:35 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config({ path:'../.env'});
 const path = require('path');
 const express = require('express')
 const mongoose = require('mongoose')
